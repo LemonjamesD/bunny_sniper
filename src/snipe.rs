4ee030c1c4486f8e36ac5203647985e47f4de929
LemonjamesD <lemon@lemonjamesd.com>
2023-10-09 16:27:04 -0400

    added a check if there are actually any deleted messages

--- src/snipe.rs
+++ src/snipe.rs
@@ -9,6 +9,11 @@ use crate::{PAST_MESSAGES, LAST_DELETED_ID};
 pub async fn snipe(ctx: &Context, msg: &Message, mut _args: Args) -> CommandResult {
     let mut locked = PAST_MESSAGES.lock().await;
     let id_locked = LAST_DELETED_ID.lock().await;
+    // Check if there are deleted messages and if there is none then say there is none
+    if locked.len() == 0 || *id_locked == 0 {
+        msg.channel_id.say(&ctx.http, "There are no cached deleted messages").await?;
+        return Ok(());
+    }
     let filtered = locked.iter().filter(|(author, content, id)| *id == *id_locked).collect::<Vec<_>>()[0];
     msg.channel_id.say(&ctx.http, format!("Sender: {}\nContext: {}\nId: {}", filtered.0, filtered.1, filtered.2)).await?;
 
