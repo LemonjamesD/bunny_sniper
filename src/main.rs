878cb31240ae46698f955ae3dd504724fe068c75
LemonjamesD <lemon@lemonjamesd.com>
2023-10-09 15:59:15 -0400

    Added base code

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,45 @@
 pub mod secrets;
 
-fn main() {
-    println!("Hello, world!");
+use secrets::TOKEN;
+
+use serenity::async_trait;
+use serenity::prelude::*;
+use serenity::model::prelude::*;
+
+use lazy_static::lazy_static;
+use tracing_subscriber::fmt::init;
+
+struct Handler;
+
+// Hold the past messages
+lazy_static! {
+    pub static ref PAST_MESSAGES: Mutex<Vec<(&'static str, &'static str)>> = Mutex::new(vec![("No one", "None")]);
+}
+
+#[async_trait]
+impl EventHandler for Handler {
+    async fn message(&self, ctx: Context, msg: Message) {
+
+    }
+
+    async fn ready(&self, _: Context, ready: Ready) {
+        println!("{} is connected!", ready.user.name);
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    // Init logging
+    init();
+
+    let intents = GatewayIntents::GUILD_MESSAGES
+        | GatewayIntents::DIRECT_MESSAGES
+        | GatewayIntents::MESSAGE_CONTENT;
+
+    let mut client =
+        Client::builder(&TOKEN, intents).event_handler(Handler).await.expect("Err creating client");
+
+    if let Err(why) = client.start().await {
+        println!("Client error: {:?}", why);
+    }
 }
