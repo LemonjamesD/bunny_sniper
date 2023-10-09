623d5557c41019df840d24c97263a77d948ce2ac
LemonjamesD <lemon@lemonjamesd.com>
2023-10-09 17:13:11 -0400

    Cleaned up code significantly and made it so there's editted and deleted message caches

--- src/main.rs
+++ src/main.rs
@@ -14,15 +14,34 @@ use tracing_subscriber::fmt::init;
 
 struct Handler;
 
-// Hold the past messages
+#[derive(Clone)]
+pub struct MessageType {
+    author: String,
+    content: String,
+    id: u64
+}
+
+impl MessageType {
+    pub fn new(author: String, content: String, id: u64) -> Self {
+        Self {
+            author,
+            content,
+            id
+        }
+    }
+}
+
 lazy_static! {
-    pub static ref PAST_MESSAGES: Mutex<Vec<(String, String, u64)>> = Mutex::new(vec![]);
+    // Hold the past messages
+    pub static ref PAST_MESSAGES: Mutex<Vec<MessageType>> = Mutex::new(vec![]);
+    pub static ref DELETED_MESSAGES: Mutex<Vec<MessageType>> = Mutex::new(vec![]);
+    pub static ref EDITTED_MESSAGES: Mutex<Vec<MessageType>> = Mutex::new(vec![]);
     pub static ref LAST_DELETED_ID: Mutex<u64> = Mutex::new(0);
     pub static ref LAST_EDITTED_ID: Mutex<u64> = Mutex::new(0);
 }
 
 // So you can't use up too much memory
-const CACHE_CAP: usize = 10;
+const CACHE_CAP: usize = 30;
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -32,32 +51,38 @@ impl EventHandler for Handler {
         if locked.len() + 1 >= CACHE_CAP {
             locked.remove(0);
         }
-        locked.push((msg.author.name.clone(), msg.content.clone(), msg.id.0));
+        locked.push(MessageType::new(msg.author.name, msg.content, msg.id.0));
     }
 
     async fn message_delete(&self, _ctx: Context, _channel_id: ChannelId, deleted_message_id: MessageId, _guild_id: Option<GuildId>,) {
-        let mut locked = LAST_DELETED_ID.lock().await;
-        *locked = deleted_message_id.0;
+        let mut deleted_id = LAST_DELETED_ID.lock().await;
+        *deleted_id = deleted_message_id.0;
+
+        // Get the deleted message to add to the deleted_messages stack
+        let past_messages = PAST_MESSAGES.lock().await;
+        let filtered = past_messages.iter().filter(|m| deleted_message_id.0 == m.id).collect::<Vec<_>>()[0];
+
+        let mut deleted_messages = DELETED_MESSAGES.lock().await;
+        deleted_messages.push(filtered.clone());
     }
 
     async fn message_update(&self, _ctx: Context, _old_if_available: Option<Message>, new: Option<Message>, event: MessageUpdateEvent) {
         // Push the new editted message onto the message stack
-        let mut locked = PAST_MESSAGES.lock().await;
+        let mut locked = EDITTED_MESSAGES.lock().await;
         // Remove unneeded cached messages
         if locked.len() + 1 >= CACHE_CAP {
             locked.remove(0);
         }
-        if let Some(new) = new {
-            locked.push((new.author.name.clone(), new.content.clone(), new.id.0));
+        // Create the MessageType before hand for less code dupe
+        let message_type = match new {
+            Some(new) => MessageType::new(new.author.name.clone(), new.content.clone(), new.id.0),
+            None => MessageType::new(event.author.unwrap().name.clone(), event.content.unwrap().clone(), event.id.0),
+        };
         
-            let mut locked = LAST_EDITTED_ID.lock().await;
-            *locked = new.id.0;
-        } else {
-            locked.push((event.author.unwrap().name.clone(), event.content.unwrap().clone(), event.id.0));
-            
-            let mut locked = LAST_EDITTED_ID.lock().await;
-            *locked = event.id.0;
-        }
+        locked.push(message_type.clone());
+    
+        let mut editted_id = LAST_EDITTED_ID.lock().await;
+        *editted_id = message_type.id;
     }
 
     async fn ready(&self, _: Context, ready: Ready) {
--- src/snipe.rs
+++ src/snipe.rs
@@ -5,7 +5,7 @@ use serenity::framework::standard::macros::{command, group};
 
 use rand::{Rng, thread_rng};
 
-use crate::{PAST_MESSAGES, LAST_DELETED_ID, LAST_EDITTED_ID};
+use crate::{LAST_DELETED_ID, LAST_EDITTED_ID, EDITTED_MESSAGES, DELETED_MESSAGES};
 
 fn random_color() -> u32 {
     let mut rng = thread_rng();
@@ -15,18 +15,18 @@ fn random_color() -> u32 {
 
 #[command]
 pub async fn snipe(ctx: &Context, msg: &Message, mut _args: Args) -> CommandResult {
-    let mut locked = PAST_MESSAGES.lock().await;
+    let mut locked = DELETED_MESSAGES.lock().await;
     let id_locked = LAST_DELETED_ID.lock().await;
     // Check if there are deleted messages and if there is none then say there is none
     if locked.len() == 0 || *id_locked == 0 {
         msg.channel_id.say(&ctx.http, "There are no cached deleted messages").await?;
         return Ok(());
     }
-    let filtered = locked.iter().filter(|(author, content, id)| *id == *id_locked).collect::<Vec<_>>()[0];
+    let filtered = locked.iter().filter(|m| m.id == *id_locked).collect::<Vec<_>>()[0];
     msg.channel_id.send_message(&ctx.http, |m| {
         m.add_embed(|e| {
-            e.field("Author", filtered.0.clone(), false)
-             .field("Message", filtered.1.clone(), false)
+            e.field("Author", filtered.author.clone(), false)
+             .field("Message", filtered.content.clone(), false)
              .color(random_color())
         })
     }).await?;
@@ -37,19 +37,19 @@ pub async fn snipe(ctx: &Context, msg: &Message, mut _args: Args) -> CommandResu
 // Basically the same thing as the snipe command but get edits
 #[command]
 pub async fn edit_snipe(ctx: &Context, msg: &Message, mut _args: Args) -> CommandResult {
-    let mut locked = PAST_MESSAGES.lock().await;
+    let mut locked = EDITTED_MESSAGES.lock().await;
     let id_locked = LAST_EDITTED_ID.lock().await;
     // Check if there are deleted messages and if there is none then say there is none
     if locked.len() == 0 || *id_locked == 0 {
         msg.channel_id.say(&ctx.http, "There are no cached deleted messages").await?;
         return Ok(());
     }
-    let filtered = locked.iter().filter(|(author, content, id)| *id == *id_locked).collect::<Vec<_>>();
+    let filtered = locked.iter().filter(|m| m.id == *id_locked).collect::<Vec<_>>();
     let filtered = filtered[filtered.len() - 1];
     msg.channel_id.send_message(&ctx.http, |m| {
         m.add_embed(|e| {
-            e.field("Author", filtered.0.clone(), false)
-             .field("Message", filtered.1.clone(), false)
+            e.field("Author", filtered.author.clone(), false)
+             .field("Message", filtered.content.clone(), false)
              .color(random_color())
         })
     }).await?;
